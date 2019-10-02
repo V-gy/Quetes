7df8b1f7d884031df9fc3a577ba32549abebaba2
V-gy <54841962+V-gy@users.noreply.github.com>
2019-10-02 09:19:27 +0200

    Update nodeJS.js

--- nodeJS.js
+++ nodeJS.js
@@ -3,7 +3,7 @@ process.stdin.setEncoding('utf8')
 
 console.log('Can you give me a number please?')
 process.stdin.on('data', (num) => {
-    if(num >= 0 && num <= 99){
+    if(num > 0 && num <= 99){
         console.log("I think you birth in " + (2019 - num));
     }else if(num > 99){
         console.log("You can't be so old !!!");
